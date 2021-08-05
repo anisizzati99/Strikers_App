9ffabc80adee59a51b3ab427b361b496f298b924
anisizzati99 <56570218+anisizzati99@users.noreply.github.com>
2021-08-06 00:31:30 +0800

    Update AboutUs.java

--- app/src/main/java/net/Strikers/strikers_app/AboutUs.java
+++ app/src/main/java/net/Strikers/strikers_app/AboutUs.java
@@ -29,7 +29,7 @@ public class AboutUs extends AppCompatActivity {
                 .addItem(new Element().setTitle("Version 1.0"))
                 .addGroup("CONNECT WITH US!")
                 .addEmail("anisizzati0108@gmil.com")
-                .addWebsite("Your website/") //github link
+                .addWebsite("https://github.com/anisizzati99/Strikers_App") //github link
                 .addYoutube("UCbekhhidkzkGryM7mi5Ys_w")   //Enter your youtube link here (replace with my channel link)
                 .addItem(createCopyright())
                 .create();
